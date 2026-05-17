dca9d27269ecbc6fc994a8ab74ad7697c20ac94c
TrqnDy <tranduyf.a3@gmail.com>
2026-05-17 20:52:58 +0700

    added gun system

--- Survivor.py
+++ Survivor.py
@@ -1,5 +1,5 @@
 from os import walk
-
+from math import atan2, degrees
 import pygame
 from player import Player
 from pytmx.util_pygame import load_pygame
@@ -69,6 +69,7 @@ class SurvivorGame:
         for obj in tmx_map.get_layer_by_name('Entities'):
             if obj.name == 'Player':
                 self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
 
     def run(self):
         while self.running:
@@ -90,7 +91,9 @@ class Gun(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         self.player = player
         self.distance = 140
-        self.player_direction = self.player.direction
+        self.WIN_WIDTH = 1280
+        self.WIN_HEIGHT = 700
+        self.player_direction = pygame.math.Vector2(0, 1)
 
         super().__init__(groups)
         self.gun_surf = pygame.image.load("gun.png").convert_alpha()
@@ -98,14 +101,22 @@ class Gun(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center = self.player.rect.center + self.player.direction * self.distance)
 
     def get_direction(self):
-        mouse_pos = pygame.mouse.get_pos()
-        player_pos = self.player.rect.center
-        self.player_direction = (mouse_pos - player_pos).normalize_ip()
+        mouse_pos = pygame.math.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.math.Vector2(self.WIN_WIDTH / 2, self.WIN_HEIGHT / 2)
+        self.player_direction = (mouse_pos - player_pos).normalize()
 
-    def rotate(self):
-        angle = self.player_direction.angle_to(pygame.math.Vector2(1, 0))
-        self.image = pygame.transform.rotate(self.gun_surf, -angle)
-        self.rect = self.image.get_rect(center = self.player.rect.center + self.player.direction * self.distance)
+    def rotate_gun(self):
+        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
+        if self.player_direction.x > 0:
+            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+            self.image = pygame.transform.flip(self.image, False, True)
+
+    def update(self, __):
+        self.get_direction()
+        self.rotate_gun()
+        self.rect.center = self.player.rect.center + self.player_direction * self.distance
 
 if __name__ == '__main__':
     game = SurvivorGame()
